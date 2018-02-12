5e71d03cae45dbdcb1e8e4b5876e871a1fd4eb68
Johannes Lamberts <mail@j-lamberts.de>
2018-02-12 10:39:55 +0100

    feat(connector): remove root files

--- connector/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CmsConnector } from './c_react/connector';
-export { default as CmsImport }    from './c_react/import';
-export { default as CmsRender }    from './c_react/renderer';
-export *                           from './models';
