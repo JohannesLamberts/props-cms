4d04ff7de6dc479d3c8d4ab949832946e04d3a97
Johannes Lamberts <mail@j-lamberts.de>
2018-02-14 17:07:26 +0100

    feat(client): add typed elementEditor - time

--- client/src/views/collection/element/editor/typeEditors/index.ts
+++ client/src/views/collection/element/editor/typeEditors/index.ts
@@ -1,14 +1,15 @@
 import { CollDefinitionFieldTypeIdent } from 'props-cms.connector-common';
-import Boolean                          from './boolean';
-import Date                             from './date';
-import Select                           from './select';
-import SelectMultiple                   from './selectMultiple';
-import SubContent                       from './subContent';
-import SubDefinition                    from './subDefinition';
-import Tags                             from './tags';
-import Text                             from './text';
-import TextArea                         from './textArea';
-import { TypeEditorComponent }          from './typeEditorProps';
+import Boolean                 from './boolean';
+import Date                    from './date';
+import Select                  from './select';
+import SelectMultiple          from './selectMultiple';
+import SubContent              from './subContent';
+import SubDefinition           from './subDefinition';
+import Tags                    from './tags';
+import Text                    from './text';
+import TextArea                from './textArea';
+import Time                    from './time';
+import { TypeEditorComponent } from './typeEditorProps';
 
 const formComponents: {
     [P in CollDefinitionFieldTypeIdent]?: TypeEditorComponent<P>
@@ -19,9 +20,10 @@ const formComponents: {
     selectMultiple: SelectMultiple,
     subContent: SubContent,
     subDefinition: SubDefinition,
+    tags: Tags,
     text: Text,
     textArea: TextArea,
-    tags: Tags
+    time: Time
 };
 
 export default formComponents;
