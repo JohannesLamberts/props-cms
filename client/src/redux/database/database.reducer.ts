cb426170817bac7e9c115a0189758cf3411e8c6f
Johannes Lamberts <mail@j-lamberts.de>
2018-02-16 21:20:10 +0100

    feat(client): restructure database reducer, use typescript-immutable

---
+++ client/src/redux/database/database.reducer.ts
@@ -0,0 +1,54 @@
+import { Collections } from 'props-cms.connector-common';
+import {
+    ImmutableMap,
+    ImmutableMapUndef,
+    ImmutableRecord
+}                      from 'typescript-immutable';
+
+const InitialCollectionState = new ImmutableRecord(
+    {
+        fullCopy: false,
+        models: new ImmutableMapUndef<string, { _id: string }>()
+    });
+
+const InitialState = new ImmutableRecord(
+    {
+        collections: new ImmutableMap<keyof Collections, typeof InitialCollectionState>(
+            InitialCollectionState)
+    });
+
+export type TState = typeof InitialState;
+
+export const DATABASE_RECIEVE = 'DATABASE_RECIEVE';
+
+export type ActionRecieve<TKey extends keyof Collections> = {
+    type: typeof DATABASE_RECIEVE;
+    payload: {
+        complete: boolean;
+        collection: TKey;
+        models: Array<Collections[TKey]>;
+    };
+};
+
+export const Reducer = (state = InitialState,
+                        action: ActionRecieve<any>): TState => {
+    switch (action.type) {
+        case DATABASE_RECIEVE: {
+            const { collection, models, complete } = action.payload;
+            return state.update('collections', collections =>
+                collections.update(collection, collectionData =>
+                    collectionData.withMutations(currValue => {
+                        const modelUpdate = {};
+                        models.forEach(model => {
+                            modelUpdate[model._id] = model;
+                        });
+                        currValue.fullCopy = currValue.fullCopy || complete;
+                        currValue.models = currValue.models.setMany(modelUpdate);
+                    })
+                )
+            );
+        }
+        default:
+            return state;
+    }
+};
