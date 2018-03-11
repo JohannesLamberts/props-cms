422a19b7ff89923e4963930b8865aa6b6d652028
Johannes Lamberts <mail@j-lamberts.de>
2018-03-11 12:28:45 +0100

    refactor(server): use websocketConnectorMongoDbWatch from server-modules

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -1,61 +1,11 @@
 import {
     ChangeStream,
     Db
-}                              from 'mongodb';
-import { WebsocketConnection } from 'server-modules';
-import { getCollection }       from './database';
-import { ENV }                 from './env';
-import { Server }              from './environment';
-
-const changeStreams: Record<string, ChangeStream> = {};
-
-const changeStream = (collectionName: string): ChangeStream => {
-    if (!changeStreams[collectionName]) {
-        changeStreams[collectionName] = getCollection(collectionName).watch();
-    }
-    return changeStreams[collectionName];
-};
-
-export class WebsocketSubscriptionConnection extends WebsocketConnection {
-
-    private _subscriptions: Record<string, Function> = {};
-
-    public init(): this {
-
-        this._socket.on('db.subscribe', (collections: string[]) => {
-            for (const collectionName of collections) {
-                if (!this._subscriptions[collectionName]) {
-                    const cb = (update) => {
-                        this._socket.emit('db.next', {
-                            collection: collectionName,
-                            operationType: update.operationType,
-                            _id: update.documentKey._id
-                        });
-                    };
-                    changeStream(collectionName).on('change', cb);
-                    this._subscriptions[collectionName] = cb;
-                }
-            }
-        });
-
-        return this;
-    }
-
-    public destroy() {
-        this._dbUnsubscribe(Object.keys(this._subscriptions));
-        return;
-    }
-
-    private _dbUnsubscribe(collectionNames: string[]) {
-        for (const collectionName of collectionNames) {
-            const cb = this._subscriptions[collectionName];
-            if (cb) {
-                changeStream(collectionName).removeListener('change', cb);
-                delete this._subscriptions[collectionName];
-            }
-        }
-    }
-}
+}                                         from 'mongodb';
+import { websocketConnectorMongoDbWatch } from 'server-modules';
+import { getCollection }                  from './database';
+import { ENV }                            from './env';
+import { Server }                         from './environment';
 
 const { websocket } = ENV;
 
@@ -65,7 +15,7 @@ export default () => {
             .createWebsocket(
                 {
                     port: websocket.port,
-                    connection: WebsocketSubscriptionConnection
+                    connection: websocketConnectorMongoDbWatch({ getCollection })
                 });
     }
 };
