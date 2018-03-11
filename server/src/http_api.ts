df1c40758e9143a369c94b4f5d6c986dd29dd75e
Johannes Lamberts <mail@j-lamberts.de>
2018-03-11 21:38:17 +0100

    feat(server): set CORS headers

--- server/src/http_api.ts
+++ server/src/http_api.ts
@@ -12,6 +12,15 @@ export default () => {
                 {
                     port: api.port,
                     init: app => {
+
+                        app.use((req, res, next) => {
+                            res.setHeader('Access-Control-Allow-Origin', '*');
+                            res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+                            res.setHeader('Access-Control-Allow-Headers',
+                                          'Origin, X-Requested-With, Content-Type, Accept');
+                            next();
+                        });
+
                         DatabaseApi.registerOn(Server.logger, app);
                         ServiceApi.registerOn(Server.logger, app);
                     }
